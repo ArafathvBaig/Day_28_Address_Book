7f6165f7af300fe5ce033db0e415cd5911f52fcc
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-22 17:07:39 +0530

    Added: .gitignore and Welcome Files to Master

---
+++ src/main/java/com/Day_28_AddressBook/WelcomeProgram.java
@@ -0,0 +1,9 @@
+package com.Day_28_AddressBook;
+
+public class WelcomeProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Day 28 Address Book Computation Problem");
+	}
+}
